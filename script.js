979e083d650a2a3b04fc3aa1142b2663416b022f
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-04-30 22:40:27 -0500

    modified pseudocode for the variable section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Here I have declared the lowercase variable. I chose to not apply an array in order to use the charAt() method later.
+//FOR ALL VARIABLE - I chose to not apply an array in order to use the charAt() method later.
+// Here I have declared the lowercase variable.
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
-// Here I have declared the uppercase variable. I chose to not apply an array in order to use the charAt() method later.
+// Here I have declared the uppercase variable.
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// Here I have declared the numeric variable. I chose to not apply an array in order to use the charAt() method later.
+// Here I have declared the numeric variable.
 var numeric = "0123456789";
