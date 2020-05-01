8fada72e59f5bb4cf28be9d987002c61d62c8e2f
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-04-30 22:39:02 -0500

    added numeric variable

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 // Here I have declared the uppercase variable. I chose to not apply an array in order to use the charAt() method later.
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Here I have declared the numeric variable. I chose to not apply an array in order to use the charAt() method later.
+var numeric = "0123456789";
