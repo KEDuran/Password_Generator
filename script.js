73ac905759abeb70906580a7aba8fd0c389728da
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-04-30 23:18:08 -0500

    Revert "added prompt for lowercase password criteria and applicable pseudocode"
    This reverts commit 3b63600829f9fb2ceba177862de5c7356cad0aca.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//FOR ALL CRITERIA VARIABLES - I chose to not apply an array in order to use the charAt() method later.
+//FOR ALL VARIABLE - I chose to not apply an array in order to use the charAt() method later.
 // Here I have declared the lowercase variable.
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 // Here I have declared the uppercase variable.
@@ -7,9 +7,3 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 // Here I have declared the special character variable.
 var specialcharacter = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-
-//All PROMPT VARIABLES - Thw variables listed below will prompt the user to select which password criteria they would lik to include in their password.
-//Here is the lowercase prompt variable.
-var lowercasePrompt = confirm(
-	"Do you want to include lowercase letters in your password?"
-);
