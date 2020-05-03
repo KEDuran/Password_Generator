bc76e2001d4b41052b82fc22042e6f2eed1528dd
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-02 23:28:12 -0500

    updated all comments to reflect changes in the code

--- script.js
+++ script.js
@@ -11,16 +11,20 @@ var numeric = "0123456789";
 // Here I declared the special character variable.
 var specialcharacter = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-/*Here I declared the password length variable that restricts the allowable number of characters that will generate to be anywhere between 8 - 128 characters.
-The logic applied to restrict character length can be found after the prompt() function. I placed a placehold interger of 8, but the full assignment logic for 
-this variable is after the passwordlengthPrompt variable.*/
+/*Here I declared the password length variable. The logic applied to restrict character length to be between 8 - 128 can be found in a while loop within
+the prompt() function. I placed a placehold interger of 0, but the full assignment logic for this variable is located with the while loop.*/
 var passwordLength = 0;
 
 /*Here I declared a master password variable with an empty string that will be used to append selected password criteria options based on user selection.
-The generatePassword() function below shows why I created this empty string variable.*/
+The generatePassword() and buildCriteria() below shows why I created this empty string variable.*/
 var master = "";
 
-/*This buildCriteria function*/
+/*This logic outlined in this buildCriteria function is used to append the password criteria variables into the master password variable based on the users answers
+to the prompting questions listed in the prompts() function. The new variables listed in this function will allow the prompting questions and user selection answers to pass
+through the logic of this function.
+*question variable is for passing the prompting question.
+*criteria variable is for passing the above password criteria.
+*choice variable if for passing the true/false value for the response.*/
 function buildCriteria(question, criteria) {
 	let choice = confirm(question);
 	if (choice) {
@@ -30,6 +34,8 @@ function buildCriteria(question, criteria) {
 	}
 }
 
+/*The generatePassword() function will generate a secure password that follows the criteria selected by the user. This function includes a for loop and establishes a 
+new local password variable that will be used to return the generated secure password that will define the securePassword variable listed at the end of the prompt() function.*/
 function generatePassword() {
 	// Declaring a local variable to apply to the for loop below. This is an empty string variable that will allow us to append selected criteria generated from the for loop.
 	let password = "";
@@ -47,36 +53,38 @@ function generatePassword() {
 	return password;
 }
 
-/*The prompts() function groups all password criteria promptvariables into a single function, which will be triggered once the user clicks the "Generate Password" button 
-in the index.html file.*/
+/*The prompts() function groups all password criteria questionts into a single function, which will be triggered once the user clicks the "Generate Password" button 
+in the index.html file. The buildCriteria() function is invoked within the prompts() function to append any user selected criteria to the empty master variable intialized in
+at the beginning of this script.*/
 function prompts() {
+	//Applying the buildCriteria() to prompting question for lowercase variable.
 	master += buildCriteria(
 		"Do you want to include LOWERCASE letters in your password?",
 		lowercase
 	);
 	console.log(master);
-
+	//Applying the buildCriteria() to prompting question for uppercase variable.
 	master += buildCriteria(
 		"Do you want to include UPPERCASE letters in your password?",
 		uppercase
 	);
 	console.log(master);
-
+	//Applying the buildCriteria() to prompting question for numeric variable.
 	master += buildCriteria(
 		"Do you want to include NUMBERS in your password?",
 		numeric
 	);
 	console.log(master);
-
+	//Applying the buildCriteria() to prompting question for specialcharacter variable.
 	master += buildCriteria(
 		"Do you want to include SPECIAL CHARACTERS in your password?",
 		specialcharacter
 	);
 	console.log(master);
 
-	/**This while loop with require the user to enter the number of characters they would like their secure password to include. Since the passwordLength variable was intialized
-	with a value of 0, user will immediately recieve an initial prompt to enter a password length after the special character prompt. If the user does not enter a password within
-	the range of allowed number of characters, they will continue to recieve the prompt below.*/
+	/**This while loop with require the user to enter how long they would like for their secure password to be. Since the passwordLength variable was intialized
+	with a value of 0, the isNan() will ensure users  recieve an initial prompt to enter a password length after the special character question. If the user does 
+	not enter a number of characters within the range of allowed number of characters, they will continue to recieve the prompt below.*/
 	while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
 		passwordLength = parseInt(
 			prompt(
@@ -91,10 +99,11 @@ function prompts() {
 	}
 	console.log(passwordLength);
 
-	/* This if-statement controls for the edge case that no password criteria is selected but the user still wants to generate a secure password. If the user does not 
+	/* This if-statement controls for the edge case that no password criteria is selected, but the user still wants to generate a secure password. If the user does not 
 	select any password criteria, this if-statement will apply all password criteria when the generatePassword() function is invoked.*/
 
-	/*NOTE - If a password length is also not selected, the password length prompt variable listed below ensures a secure random password of at least 8 characters is generated*/
+	/*NOTE - The while loop for the passwordLength variable above ensures the user is prompted to specify how long they would like their secure password to be, so
+	a numerical value of total desired characters will be defined when this prompts() function is invoked.*/
 
 	if (master === "") {
 		master = master + lowercase + uppercase + numeric + specialcharacter;
@@ -106,8 +115,11 @@ function prompts() {
 
 	//This is the statement that will output the generated password into the app's user interface.
 	document.getElementById("securepass").innerHTML = securePassword;
-	//
 
+	/*Resetting the passwordLength variable to 0 to ensure that the password length resets after the prompts() function finished running and/or the user want to generate a new 
+	password and re-clicks the "Generate Password" button. I did this because I noticed some edge cases where the password length would not reset unless the web page
+	was completely refreshed.*/
 	passwordLength = 0;
+	//Resetting the master variable to an empty string to control for the above edge case describe for passwordLength variable.
 	master = "";
 }
